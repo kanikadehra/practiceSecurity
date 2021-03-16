9dd8ad8a6329016910fa7f3c2178bdcefce54c21
Ace <ace120387@gmail.com>
2021-03-15 20:34:11 -0400

    fixed the error

--- src/main/java/com/kanika/practiceSecurity/security/ApplicationUserRole.java
+++ src/main/java/com/kanika/practiceSecurity/security/ApplicationUserRole.java
@@ -29,7 +29,8 @@ public enum ApplicationUserRole {
         Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
                 .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
                 .collect(Collectors.toSet());
-        permissions.add(new SimpleGrantedAuthority("Role_" +this.name()));
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_" +this.name()));
         return permissions;
     }
 }
